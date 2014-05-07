ce8daea88c471a54368a5ded257d549ae1ffb231
Ming Chen <mingchen1989@gmail.com>
2014-05-06 20:26:29 -0500

    accept number of clients as argument

--- server.py
+++ server.py
@@ -10,6 +10,22 @@ import boto.dynamodb2
 import socket, time
 import errno
 
+# get all running hosts
+def get_hosts(state='running'):
+    connection = boto.ec2.connect_to_region(config.region)
+
+    role = config.role 
+    hosts = []
+    for reservation in connection.get_all_instances():
+        for instance in reservation.instances:
+            if instance.state == state:
+                for key in ['role']:
+                    value = instance.tags.get(key)
+                    if value and role in value.split(','):
+                        hosts.append(instance.public_dns_name)
+                        break
+    return hosts
+
 def main(argv):
     
     conn = boto.dynamodb2.connect_to_region(
@@ -20,10 +36,15 @@ def main(argv):
         table=config.table_name,
         conn=conn
     )
-
+    
+    if len(argv) != 3:
+        print 'usage: python program.py workload number_instances'
+        return
     workload = int(argv[1])
-    # hosts = config.get_hosts()
-    hosts = ['127.0.0.1', ]
+    number_of_instances = int(argv[2])
+    hosts = get_hosts()
+    # print hosts
+    # hosts = ['127.0.0.1', ]
     
     params = {'nRequests':(workload/(len(hosts))), 'setup':0}
     c = BenchmarkConfig(handle, **params)
@@ -35,6 +56,9 @@ def main(argv):
     begin = time.time()
 
     for idx, host in enumerate(hosts):
+        # start as many clients as we want
+        if idx >= number_of_instances:
+            break
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((host, config.port))
